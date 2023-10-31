bba3ff989831cabde23c8dd9fb5abc6674cdfbb7
armgp <arm.gp23@gmail.com>
2023-10-31 21:11:46 +0530

    added certifications

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -134,6 +134,86 @@ const TAB_DATA = [
                 </li>
             </ul>
         )
+    },
+    {
+        title: "Certifications",
+        id: "certifications",
+        content: (
+            <ul className='list-disc'>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.credly.com/badges/9f98423d-717e-4f45-bf24-910f52b92c40/public_url'
+                        target="_blank"
+                    >
+                        Snowflake Hands On Essentials - Data Applications
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.credly.com/badges/bed25db9-4070-4056-8c43-9ec1d1d6e694/public_url'
+                        target="_blank"
+                    >
+                        Snowflake Hands On Essentials - Data Sharing
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.credly.com/badges/e796f807-630d-4c73-a66d-f9e504e883cb/public_url'
+                        target="_blank"
+                    >
+                        Snowflake Hands On Essentials - Data Warehouse
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.credly.com/badges/167a0548-b087-4111-a452-48721729735a/public_url'
+                        target="_blank"
+                    >
+                        Snowflake Hands On Essentials - Data Lake
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.coursera.org/account/accomplishments/verify/6UFGWW5WMWJR'
+                        target="_blank"
+                    >
+                        Convolutional Neural Networks
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.coursera.org/account/accomplishments/verify/LDEYU5RWAGJB'
+                        target="_blank"
+                    >
+                        Structuring Machine Learning Projects
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.coursera.org/account/accomplishments/verify/7PZ6LQULJD2C'
+                        target="_blank"
+                    >
+                        Improving Deep Neural Networks: Hyperparameter Tuning, Regularization and Optimization
+                    </a>
+                </li>
+                <li>
+                    <a
+                        className='font-semibold underline hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r from-primary-700 via-black-500 to-primary-800  hover:bg-primary-800'
+                        href='https://www.coursera.org/account/accomplishments/verify/K3JC5KRA9SYB'
+                        target="_blank"
+                    >
+                        Neural Networks and Deep Learning
+                    </a>
+                </li>
+            </ul>
+        )
     }
     
 ]
